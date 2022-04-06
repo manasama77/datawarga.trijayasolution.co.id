efef677c782af89863c8bf4c6a16c06a55c2aded
Adam PM <adam.pm77@gmail.com>
2022-04-06 21:26:18 +0700

    M

--- pages/_partials/sidebar.php
+++ pages/_partials/sidebar.php
@@ -52,7 +52,7 @@ function is_active($page)
         <div class="list-group">
           <!-- <a href="../kelahiran" class="list-group-item"><i class="fa fa-venus-mars fa-fw"></i> Kelahiran</a> -->
           <!-- <a href="../meninggal" class="list-group-item"><i class="fa fa-ambulance fa-fw"></i> Kematian</a> -->
-          <a href="../peristiwa-Kelahiran" class="list-group-item">Kelahiran</a>
+          <a href="../peristiwa-kelahiran" class="list-group-item">Kelahiran</a>
           <a href="../peristiwa-kematian" class="list-group-item">Kematian</a>
           <a href="../peristiwa-kerja-luar_negeri-atau-kota" class="list-group-item">Bekerja Luar Negeri / Kota</a>
           <a href="../peristiwa-belum-bekerja" class="list-group-item">Belum Bekerja</a>
