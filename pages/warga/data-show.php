5fc3322868edf24365880d5529ee74d653f75738
Adam PM <adam.pm77@gmail.com>
2022-05-15 22:02:12 +0700

    perbaikan warga get query data KK

--- pages/warga/data-show.php
+++ pages/warga/data-show.php
@@ -8,12 +8,7 @@ $get_id_warga = $_GET['id_warga'];
 $query = "SELECT * FROM warga WHERE id_warga = $get_id_warga";
 # $query_kk = "SELECT * FROM warga JOIN kartu_keluarga ON warga.id_warga=kartu_keluarga.id_kepala_keluarga WHERE warga.id_warga=$get_id_warga";
 
-$query_kk = "SELECT * FROM warga left JOIN warga_has_kartu_keluarga ON warga_has_kartu_keluarga.id_warga=warga.id_warga LEFT JOIN kartu_keluarga on kartu_keluarga.id_keluarga=warga_has_kartu_keluarga.id_keluarga WHERE warga.id_warga=$get_id_warga";
-
-#$query_kk = "SELECT * FROM warga left JOIN warga_has_kartu_keluarga ON warga_has_kartu_keluarga.id_warga=warga.id_warga WHERE warga.id_warga=$get_id_warga";
-
 $hasil = mysqli_query($db, $query);
-$hasil_kk = mysqli_query($db, $query_kk);
 
 $data_warga = array();
 $data_kk = array();
@@ -22,6 +17,28 @@ while ($row = mysqli_fetch_assoc($hasil)) {
   $data_warga[] = $row;
 }
 
-while ($row = mysqli_fetch_assoc($hasil_kk)) {
-  $data_kk[] = $row;
+$sql_1   = "select * from kartu_keluarga where kartu_keluarga.id_kepala_keluarga = $get_id_warga";
+$query_1 = mysqli_query($db, $sql_1);
+$num_rows_1 = mysqli_num_rows($query_1);
+
+if ($num_rows_1 > 0) {
+  while ($row = mysqli_fetch_assoc($query_1)) {
+    $data_kk[] = $row;
+  }
+} else {
+  $sql_2   = "
+    SELECT
+    kartu_keluarga.* 
+  FROM
+    warga_has_kartu_keluarga 
+    left join kartu_keluarga on kartu_keluarga.id_keluarga = warga_has_kartu_keluarga.id_keluarga
+  WHERE
+    id_warga = $get_id_warga
+  ";
+
+  $query_2    = mysqli_query($db, $sql_2);
+
+  while ($row = mysqli_fetch_assoc($query_2)) {
+    $data_kk[] = $row;
+  }
 }
